f24369ee9e618aa098867632c08a53277a0fb48c
이상훈 <tkdgns25300@gmail.com>
2025-08-24 23:59:04 +0900

    initial setting

---
+++ src/main/java/sanghun/project/uncomfortablehub/UncomfortablehubApplication.java
@@ -0,0 +1,13 @@
+package sanghun.project.uncomfortablehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UncomfortablehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UncomfortablehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sanghun/project/uncomfortablehub/UncomfortablehubApplicationTests.java
@@ -0,0 +1,13 @@
+package sanghun.project.uncomfortablehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UncomfortablehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
